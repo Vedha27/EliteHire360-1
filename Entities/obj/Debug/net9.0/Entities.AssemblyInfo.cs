b8d9d838923c35f09f4fc48f5804914050d30c31
Nithi230 <nikenithi123@gmail.com>
2025-10-24 12:45:58 +0530

    n

--- Entities/obj/Debug/net9.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net9.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4def5564f0ba07c993600808141f939f409fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9003ab49e20d39a39331737bc2176235d7a0bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
